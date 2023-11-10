60839252e9294d6d6a6ddd1790080506412b4082
GABRIEL VANZELLI LEITE <gabriel.vleite1@senacsp.edu.br>
2023-11-10 11:11:50 -0300

    a

--- conteudo/destaque.php
+++ conteudo/destaque.php
@@ -1,7 +1,7 @@
 <section class="destaque wow animate__animated animate__fadeInUp">
             <div class="site">
                 <a href="https://www.youtube.com/watch?v=167d3nSLrPE" data-lity>
-                    <img src="img/icones/iconePlay.svg" alt="Play do video da academia">
+                    <img src="img/icones/iconePlay.svg" alt="Playy do video da academia">
                 </a>
             </div>
 
